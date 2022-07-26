924235c6decb6bb8ac219c298eb3016fd23c4832
jonas <jonas_barros@outlook.com>
2022-07-26 19:54:43 -0300

    alterando tipo de lista de pedidos para ArrayList

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -1,8 +1,11 @@
 package br.com.alura.comex;
 
+import java.util.ArrayList;
+
 public class Main {
 
   public static void main(String[] args) {
-    Pedido[] pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
+    ArrayList<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
+    System.out.println(pedidos);
   }
 }
