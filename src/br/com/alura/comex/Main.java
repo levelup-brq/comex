61ad044c83897fbebcdc0eb16ed06b6dde770dc5
Jonas Barros <jonas_barros@outlook.com>
2022-08-05 21:05:00 -0300

    concluindo o exercício do relatório de número de pedidos por categoria

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -1,7 +1,9 @@
 package br.com.alura.comex;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -18,8 +20,24 @@ public class Main {
     }
 
     for (String categoria : categoriasSemDuplicacao) {
-     System.out.println(categoria);
+     //System.out.println(categoria);
     }
+
+
+    /**
+     * Relatório de número de pedidos por categoria
+     */
+    Map<String, Long> categoriasSemDuplicoes = pedidos.stream()
+      .collect(
+        Collectors
+          .groupingBy(Pedido::getCategoria, Collectors.counting()));
+          
+
+    for (Map.Entry<String, Long> pedido : categoriasSemDuplicoes.entrySet()) {
+      String pedidoFormatado = String.format("%s: %s", pedido.getKey(), pedido.getValue());
+      System.out.println(pedidoFormatado);
+    }
+
 
   }
 }
