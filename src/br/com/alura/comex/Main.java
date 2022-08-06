8d9dd49dfbfb8a2e12e3c4946dc87e6d9edd9786
Jonas Barros <jonas_barros@outlook.com>
2022-08-05 21:00:55 -0300

    concluindo o exercicio de lista de categorias sem duplicações

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -1,10 +1,25 @@
 package br.com.alura.comex;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
   public static void main(String[] args) {
     List<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
+
+    /**
+     * Lista de Categoria sem duplicação
+     */
+    Set<String> categoriasSemDuplicacao = new HashSet<>();
+
+    for (Pedido pedido : pedidos) {
+      categoriasSemDuplicacao.add(pedido.getCategoria());
+    }
+
+    for (String categoria : categoriasSemDuplicacao) {
+     System.out.println(categoria);
+    }
 
   }
 }
