50f65e3b5a6ddf42e2384a55c083783b86658c4b
dhms <douglas.heber.ms@gmail.com>
2022-08-08 19:34:49 -0300

    respostas semana 2

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -3,6 +3,7 @@ package br.com.alura.comex;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -10,33 +11,36 @@ public class Main {
 	public static void main(String[] args) {
 		List<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
 
-		Set<String> categoriasSemDuplicacao = new HashSet<>();
-		Set<String> nomesOrdenados = new HashSet<>();
-
+		//Set<String> categoriasSemDuplicacao = new HashSet<>();
+		//Set<String> nomesOrdenados = new HashSet<>();
+		
+		/*
 		pedidos.stream().forEach(p -> {
 			categoriasSemDuplicacao.add(p.getCategoria());
 			nomesOrdenados.add(p.getCliente());
 		});
 
-		/*
+		
 		 * for (Pedido p : pedidos) { nomesOrdenados.add(p.getCliente());
 		 * categoriasSemDuplicacao.add(p.getCategoria()); }
 		 */
-
+		
+		pedidos.stream().map(Pedido::getCategoria).collect(Collectors.toSet()).forEach(System.out::println);
 		// Quebra de Linha
 		System.out.println("");
 		System.out.println("Lista de categorias sem duplica��es");
-		categoriasSemDuplicacao.stream().forEach(c -> System.out.println(c));
-
+		//categoriasSemDuplicacao.stream().forEach(c -> System.out.println(c));
+		
+		pedidos.stream().map(Pedido::getCliente).collect(Collectors.toCollection( TreeSet::new )).forEach(System.out::println);
 		// Quebra de Linha
 		System.out.println("");
 		System.out.println("Lista ordenada de clientes sem duplica��es");
-		nomesOrdenados.stream().sorted((n1, n2) -> n1.compareTo(n2)).forEach(n -> System.out.println(n));
+		//nomesOrdenados.stream().sorted((n1, n2) -> n1.compareTo(n2)).forEach(n -> System.out.println(n));
 
 		// Quebra de Linha
 		System.out.println("");
 		System.out.println("Relat�rio de n�mero de pedidos por categoria");
-		pedidos.stream().collect(Collectors.groupingBy(p -> p.getCategoria(), Collectors.counting()))
+		pedidos.stream().collect(Collectors.groupingBy(Pedido::getCategoria, Collectors.counting()))
 				.forEach((categoria, qtd) -> System.out.println(categoria + " : " + qtd));
 	}
 }
