41903e5d64b871622ed381b5a47ce40cd634e5f8
VagnerUrata <vagnerurata@gmail.com>
2022-08-08 16:46:31 -0300

    Lista de categorias sem duplicações

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -1,8 +1,23 @@
 package br.com.alura.comex;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
 
   public static void main(String[] args) {
-    Pedido[] pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
+    List<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
+
+    List<String> listaClientes = new ArrayList<>();
+
+    Set<String> conjuntoCategorias = new HashSet<>();
+    for (Pedido pedido : pedidos) {
+      conjuntoCategorias.add(pedido.getCategoria());
+    }
+    for (String categoria : conjuntoCategorias) {
+      System.out.println(categoria);
+    }
   }
 }
