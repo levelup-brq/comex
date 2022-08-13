a7f05a5c09276c3208f0167a77317df4a110c71d
Jonas Barros <jonas_barros@outlook.com>
2022-08-13 09:23:08 -0300

    removendo os exercicios da semana 2

--- src/br/com/alura/comex/Main.java
+++ src/br/com/alura/comex/Main.java
@@ -1,55 +1,10 @@
 package br.com.alura.comex;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 public class Main {
 
   public static void main(String[] args) {
     List<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
-
-    /**
-     * Lista de Categoria sem duplicação
-     */
-    Set<String> categoriasSemDuplicacao = new HashSet<>();
-
-    for (Pedido pedido : pedidos) {
-      categoriasSemDuplicacao.add(pedido.getCategoria());
-    }
-
-    for (String categoria : categoriasSemDuplicacao) {
-     System.out.println(categoria);
-    }
-
-
-    /**
-     * Relatório de número de pedidos por categoria
-     * 
-     */
-    Map<String, Long> categoriasSemDuplicoes = pedidos.stream()
-      .collect(
-        Collectors
-          .groupingBy(Pedido::getCategoria, Collectors.counting()));
-          
-
-    for (Map.Entry<String, Long> pedido : categoriasSemDuplicoes.entrySet()) {
-      String pedidoFormatado = String.format("%s: %s", pedido.getKey(), pedido.getValue());
-      System.out.println(pedidoFormatado);
-    }
-
-
-    /**
-     * Lista de clientes ordenados sem duplicações
-     *
-     */
-
-    pedidos.stream()
-      .map(Pedido::getCliente)
-      .collect(Collectors.toCollection(TreeSet::new))
-      .forEach(System.out::println);
 
   }
 }
