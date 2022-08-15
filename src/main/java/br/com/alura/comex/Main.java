99c0327d3f04ab60e25e22e96bc70fff07939a80
VagnerUrata <vagnerurata@gmail.com>
2022-08-15 19:23:24 -0300

    Utilizando uma biblioteca para ler o CSV de pedidos

--- src/main/java/br/com/alura/comex/Main.java
+++ src/main/java/br/com/alura/comex/Main.java
@@ -1,8 +1,40 @@
 package main.java.br.com.alura.comex;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
 
   public static void main(String[] args) {
-    Pedido[] pedidos = ProcessadorDeCsv.processaArquivo("resources/pedidos.csv");
+    List<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("resources/pedidos.csv");
+
+    List<String> listaClientes = new ArrayList<>();
+
+    Set<String> conjuntoCategorias = new HashSet<>();
+    for (Pedido pedido : pedidos) {
+      conjuntoCategorias.add(pedido.getCategoria());
+    }
+    for (String categoria : conjuntoCategorias) {
+      System.out.println(categoria);
+    }
+
+    System.out.println("\n");
+
+    Map<String, Long> numeroPedidosPorCategoria = pedidos.stream().collect(Collectors.groupingBy(Pedido::getCategoria, Collectors.counting()));
+
+    for (Map.Entry<String, Long> pedido : numeroPedidosPorCategoria.entrySet()) {
+      System.out.println(pedido.getKey() + ": " + pedido.getValue());
+    }
+
+    System.out.println("\n");
+
+    SortedSet<String> listaClientesOrdenados = new TreeSet<>(listaClientes);
+    for (Pedido pedido : pedidos) {
+      listaClientesOrdenados.add(pedido.getCliente());
+    }
+    for (String cliente : listaClientesOrdenados) {
+      System.out.println(cliente);
+    }
   }
+
 }
