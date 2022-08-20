ad49f396a4145b5081623fc87aeb2ad76534e2e3
Jonas Barros <jonas_barros@outlook.com>
2022-08-19 21:26:16 -0300

    removendo os exercícios da semana 3

--- src/main/java/br/com/alura/comex/Main.java
+++ src/main/java/br/com/alura/comex/Main.java
@@ -1,18 +1,9 @@
 package br.com.alura.comex;
-import java.util.List;
 
 public class Main 
 {
     public static void main( String[] args )
     {
-        //List<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
-        List<Pedido> pedidos = ProcessadorDeCsvLibExterna.processaArquivo("pedidos.csv");
-        //List<Pedido> pedidos = ProcessadorDeXml.processaArquivo("pedidos.xml");
-
-        for (Pedido pedido : pedidos) {
-            String relatorio = String.format("%s %s %s", pedido.getCategoria(), pedido.getData(), pedido.getCliente());
-            System.out.println(relatorio);
-        }
 
     }
 }
