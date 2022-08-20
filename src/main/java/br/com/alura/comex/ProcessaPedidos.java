783da77612e01ddc38f76c5636f3a1791f81a468
Jonas Barros <jonas_barros@outlook.com>
2022-08-19 22:21:53 -0300

    concluindo o exericicio - teste de unidade para lista de categorias sem duplicacoes

--- src/main/java/br/com/alura/comex/ProcessaPedidos.java
+++ src/main/java/br/com/alura/comex/ProcessaPedidos.java
@@ -1,10 +1,17 @@
 package br.com.alura.comex;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ProcessaPedidos {
+
+  List<Pedido> pedidos;
+
+  ProcessaPedidos() {
+    this.pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
+  }
 
   public List<String> nomeDosClientes() {
     List<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("pedidos.csv");
@@ -13,6 +20,16 @@ public class ProcessaPedidos {
       .stream()
       .map(Pedido::getCliente)
       .collect(Collectors.toList());
+  }
+
+  public Set<String> categoriasSemDuplicacoes() {
+    Set<String> categoriasSemDuplicacao = new HashSet<>();
+    
+    this.pedidos
+      .stream()
+      .forEach(pedido -> categoriasSemDuplicacao.add(pedido.getCategoria()));
+    
+    return categoriasSemDuplicacao;
   }
 
 }
--- src/test/java/br/com/alura/comex/ProcessaPedidosTest.java
+++ src/test/java/br/com/alura/comex/ProcessaPedidosTest.java
@@ -1,7 +1,9 @@
 package br.com.alura.comex;
 
+import java.util.HashSet;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +14,18 @@ class ProcessaPedidosTest {
     ProcessaPedidos processaPedidos = new ProcessaPedidos();
     List<String> nomeDosClientes = processaPedidos.nomeDosClientes();
     assertFalse(nomeDosClientes.isEmpty());
+  }
+
+  @Test
+  void deveRetornarUmaListaDeCategoriasSemDuplicacoesNaoVazia() {
+    ProcessaPedidos processaPedidos = new ProcessaPedidos();
+    assertEquals(processaPedidos.categoriasSemDuplicacoes().getClass(), HashSet.class);
+  }
+
+  @Test
+  void metodoCategoriasSemDuplicacoesDeveRetornarUmHashSet() {
+    ProcessaPedidos processaPedidos = new ProcessaPedidos();
+    assertEquals(processaPedidos.categoriasSemDuplicacoes().getClass(), HashSet.class);
   }
 
 }
