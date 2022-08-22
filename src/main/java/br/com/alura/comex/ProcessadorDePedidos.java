3165997fa2fcb3f606aae13dd89a6b10464878c1
VagnerUrata <vagnerurata@gmail.com>
2022-08-22 20:54:27 -0300

    Atualizado projeto testes

--- src/main/java/br/com/alura/comex/ProcessadorDePedidos.java
+++ src/main/java/br/com/alura/comex/ProcessadorDePedidos.java
@@ -3,12 +3,11 @@ package br.com.alura.comex;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class PedidoService {
+public class ProcessadorDePedidos {
 
 	public List<String> nomesClientes() {
 		List<Pedido> pedidos = ProcessadorDeCsv.processaArquivo("resources/pedidos.csv");
 
 		return pedidos.stream().map(Pedido::getCliente).collect(Collectors.toList());
 	}
-
 }
