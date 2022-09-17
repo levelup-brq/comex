3a20ca8163aec3d1ed5b0f0e3044982ab4384c74
Jonas Barros <jonas_barros@outlook.com>
2022-09-17 14:54:50 -0300

    criando classes DTO

---
+++ src/main/java/br/com/alura/comex/controller/dto/CategoriaDTO.java
@@ -0,0 +1,50 @@
+package br.com.alura.comex.controller.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.comex.modelo.Categoria;
+import br.com.alura.comex.modelo.StatusCategoria;
+
+public class CategoriaDTO {
+
+  CategoriaDTO(Categoria categoria) {
+    this.id = categoria.getId();
+    this.nome = categoria.getNome();
+    this.status = categoria.getStatus();
+  }
+
+  private Long id;
+  private String nome;
+  private StatusCategoria status = StatusCategoria.ATIVA;  
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public StatusCategoria getStatus() {
+    return status;
+  }
+
+  public void setStatus(StatusCategoria status) {
+    this.status = status;
+  }
+
+  public static List<CategoriaDTO> converter(List<Categoria> categorias) {
+    return categorias.stream()
+      .map(CategoriaDTO::new)
+      .collect(Collectors.toList());
+  }
+}
---
+++ src/main/java/br/com/alura/comex/controller/dto/ClientesDTO.java
@@ -0,0 +1,57 @@
+package br.com.alura.comex.controller.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.comex.modelo.Cliente;
+import br.com.alura.comex.modelo.StatusDoCliente;
+
+public class ClientesDTO {
+
+  private Long id;
+  private String nome;
+  private String cpf;
+  private String email;
+  private String local;
+  private StatusDoCliente status; 
+
+  public ClientesDTO(Cliente cliente) {
+    this.cpf = cliente.getCpf();
+    this.nome = cliente.getNome();
+    this.email = cliente.getEmail();
+    this.id = cliente.getId();
+    this.status = cliente.getStatus();
+    this.local = cliente.getEndereco().local();
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getLocal() {
+    return local;
+  }
+
+  public StatusDoCliente getStatus() {
+    return status;
+  }
+
+  public static List<ClientesDTO> converter(List<Cliente> clientes) {
+    return clientes.stream()
+      .map(ClientesDTO::new)
+      .collect(Collectors.toList());
+  }
+
+}
