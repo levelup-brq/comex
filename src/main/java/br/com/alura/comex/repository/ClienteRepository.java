e1310d3a680f13ea6ce0c0fbb2baddd7b6bc4ad8
Jonas Barros <jonas_barros@outlook.com>
2022-09-17 14:55:48 -0300

    alterando tipo da interface

--- src/main/java/br/com/alura/comex/repository/ClienteRepository.java
+++ src/main/java/br/com/alura/comex/repository/ClienteRepository.java
@@ -2,6 +2,7 @@ package br.com.alura.comex.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,7 @@ import br.com.alura.comex.modelo.Cliente;
 import br.com.alura.comex.modelo.StatusDoCliente;
 
 @Repository
-public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
   
   @Query("SELECT c FROM Cliente c WHERE c.nome = :nome")
   Cliente buscarPorNome(String nome);
