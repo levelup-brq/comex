06500ea0819a55fd5fcc255384bbce0b9ab2d9e5
Jonas Barros <jonas_barros@outlook.com>
2022-09-23 21:11:13 -0300

    criando gerenciamento de cache para cliente

--- src/main/java/br/com/alura/comex/ComexApplication.java
+++ src/main/java/br/com/alura/comex/ComexApplication.java
@@ -2,9 +2,11 @@ package br.com.alura.comex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
+@EnableCaching
 public class ComexApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/alura/comex/controller/ClientesController.java
+++ src/main/java/br/com/alura/comex/controller/ClientesController.java
@@ -3,12 +3,15 @@ package br.com.alura.comex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.validation.Valid;
 
 import br.com.alura.comex.controller.dto.ClientesDTO;
@@ -24,12 +27,14 @@ public class ClientesController {
   private ClienteRepository clienteRepository;
 
   @GetMapping
+  @Cacheable(value = "listaDeClientes")
   public List<ClientesDTO> lista() {
     List<Cliente> clientes = this.clienteRepository.findAll();
     return ClientesDTO.converter(clientes);
   }
 
   @PostMapping
+  @CacheEvict(value = "listaDeClientes", allEntries = true)
   public ResponseEntity<ClienteForm> cadastrar(@RequestBody @Valid ClienteForm clienteForm) {
     Cliente cliente = clienteForm.converter();
     this.clienteRepository.save(cliente);
